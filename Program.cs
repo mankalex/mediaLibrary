51279e89e153e2c858fd48480a8a223a492e7302
alexm <amanka@my.wctc.edu>
2023-03-13 13:19:55 -0500

    [1.08] Create File Scrubber

--- Program.cs
+++ Program.cs
@@ -7,30 +7,7 @@ string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
 
-Movie movie = new Movie
-{
-    mediaId = 123,
-    title = "Greatest Movie Ever, The (2023)",
-    director = "Jeff Grissom",
-    // timespan (hours, minutes, seconds)
-    runningTime = new TimeSpan(2, 21, 23),
-    genres = { "Comedy", "Romance" }
-    
-};
-
-Console.WriteLine(movie.Display());
-
-
-Book book = new Book
-{
-    mediaId = 111,
-    title = "Super Cool Book",
-    author = "Jeff Grissom",
-    pageCount = 101,
-    publisher = "",
-    genres = { "Suspense", "Mystery" }
-};
-Console.WriteLine(book.Display());
-
+string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+logger.Info(scrubbedFile);
 
 logger.Info("Program ended");
